f2f6f099d1b1b3e205c640066834877bd84a4566
Candex22 <candemolinari20@gmail.com>
2025-04-02 23:11:05 -0300

    fixed menu path

--- components/menu.php
+++ components/menu.php
@@ -1,7 +1,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            <a class="navbar-brand" href="index.php">
+            <a class="navbar-brand" href="/PA-OL_T3/index.php">
                 <i class="fas fa-tools me-2"></i>
                 Sistema de Pañol
             </a>
@@ -11,7 +11,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav me-auto">
                     <li class="nav-item">
-                        <a class="nav-link active" href="index.php">Inicio</a>
+                        <a class="nav-link active" href="/PA-OL_T3/index.php">Inicio</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="components/herramientas_lista.php">Herramientas</a>
